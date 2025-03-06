d429c4622320f15658173cef7ab1be159e50192e
socomplexy <socomplexy@gmail.com>
2025-03-06 14:00:52 +0000

    scroll to top an hash routing

--- src/App.js
+++ src/App.js
@@ -1,61 +1,50 @@
-import { useState } from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { useLayoutEffect } from "react";
+import { HashRouter, Route, Routes, useLocation } from "react-router-dom";
 import "./Styles/App.scss";
 import { Components } from "./Components";
 import { Views } from "./Views";
 
-function App() {
-  const [isOpen, setIsOpen] = useState(false);
-  const toggleIsOpen = (status) => setIsOpen(status);
+const Inner = () => {
+  const location = useLocation();
 
-  const [view, setView] = useState("Home");
-  const updateView = (newView) => {
-    newView !== view && setView(newView);
-    toggleIsOpen(false);
-  };
+  // scroll to top of page after a page transition.
+  useLayoutEffect(() => {
+    document.documentElement.scrollTo({ top: 0, left: 0, behavior: "instant" });
+  }, [location.pathname]);
 
   return (
-    <div className="App">
-      <Router>
-        {/* <Components.Header onClick={toggleIsOpen} isOpen={isOpen} /> */}
-        <Components.AppBar />
-        <Routes>
-          <Route path="/base-practice" element={<Views.DNABasePractice />} />
-          <Route path="/genetics" element={<Views.Genetics />} />
-          <Route path="/molarity" element={<Views.Molarity />} />
-          <Route
-            path="/understanding-molarity"
-            element={<Views.UnderstandingMolarity />}
-          />
-          <Route
-            path="/molarity-practice"
-            element={<Views.MolarityPractice />}
-          />
+    <Routes>
+      <Route path="/base-practice" element={<Views.DNABasePractice />} />
+      <Route path="/genetics" element={<Views.Genetics />} />
+      <Route path="/molarity" element={<Views.Molarity />} />
+      <Route
+        path="/understanding-molarity"
+        element={<Views.UnderstandingMolarity />}
+      />
+      <Route path="/molarity-practice" element={<Views.MolarityPractice />} />
 
-          <Route
-            path="/EquationPractice"
-            element={<Views.EquationPractice />}
-          />
-          <Route
-            path="/MolarityConversion"
-            element={<Views.MolarityConversion />}
-          />
-          <Route
-            path="/MoleculeGenerator"
-            element={<Views.MoleculeGenerator />}
-          />
-          <Route
-            path="/PedigreePractice"
-            element={<Views.PedigreePractice />}
-          />
-          <Route
-            path="/PunnettSquarePractice"
-            element={<Views.PunnettSquarePractice />}
-          />
-          <Route path="/" element={<Views.Home />} />
-        </Routes>
-      </Router>
-    </div>
+      <Route path="/EquationPractice" element={<Views.EquationPractice />} />
+      <Route
+        path="/MolarityConversion"
+        element={<Views.MolarityConversion />}
+      />
+      <Route path="/MoleculeGenerator" element={<Views.MoleculeGenerator />} />
+      <Route path="/PedigreePractice" element={<Views.PedigreePractice />} />
+      <Route
+        path="/PunnettSquarePractice"
+        element={<Views.PunnettSquarePractice />}
+      />
+      <Route path="/" element={<Views.Home />} />
+    </Routes>
+  );
+};
+
+function App() {
+  return (
+    <HashRouter className="App">
+      <Components.AppBar />
+      <Inner />
+    </HashRouter>
   );
 }
 
